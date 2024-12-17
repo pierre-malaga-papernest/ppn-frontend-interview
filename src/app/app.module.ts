1c22064bcad19adb77d1f657d4541b088033040c
Dani Villanueva <danielvillanueva94@gmail.com>
2024-12-17 16:44:12 +0100

    Fix providers and import httpClientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { EffectsModule } from '@ngrx/effects';
 import { AppComponent } from './app.component';
 import { UserEffects } from './store/effects';
 import { userReducer } from './store/reducer';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
+    HttpClientModule,
     StoreModule.forRoot({ user: userReducer }),
     EffectsModule.forRoot([UserEffects]),
   ],
